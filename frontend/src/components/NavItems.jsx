973e0aa6f916d48dbb13f674dc517b04bbf1dedc
Elhadj Tall <elhadjoumartall10@gmail.com>
2024-12-05 00:04:12 +0100

    AJout du NavItems

--- frontend/src/components/NavItems.jsx
+++ frontend/src/components/NavItems.jsx
@@ -1,55 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+const NavItems = () => {
+  const [menuToggle, setMenuToggle] = useState(false);
+  const [socialToggle, setSocialToggle] = useState(false);
+  const [headerFixed, setHeaderFixed] = useState(false);
 
-const navItems = () => {
-    
+  // Add event listener for scroll
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+      setHeaderFixed(true);
+    } else {
+      setHeaderFixed(false);
+    }
+  });
 
-    const [menuToggle, setMenuToggle] = useState(false);
-    const [socialToggle, setSocialToggle] = useState(false);
-    const [HeaderFixed, setHeaderFixed] = useState(false);
+  return (
+    <header>
+      <div className="hedear-top">
+        <div className="container">
+          <div className="header-top-area">
+            <Link to="/signup" className="lab-btn me-3">
+              <span>Create Account</span>
+            </Link>
+            <Link to="/login">
+              <span>Log In</span>
+            </Link>
+          </div>
+        </div>
+      </div>
 
-    //  addevent listener
-    window.addEventListener("scroll", () => {
-        if (window.scrollY > 200) {
-            setHeaderFixed(true);
-        } else {
-            setHeaderFixed(false);
-        }
-    })
-
-    return (
-        <header>
-            <div className="hedear-top">
-                <div className="container">
-                    <div className="header-top-area">
-                        <Link to="/signup" className='lab-btn me-3'><span>Create Account</span></Link>
-                        <Link to="/login"><span>Log In</span></Link>
-                    </div>
+      {/* Header Bottom */}
+      <div className="header-bottom">
+        <div className="container">
+          <div className="header-wrapper">
+            {/* Logo */}
+            <div className="logo">
+              <div className="logo-search-acte">
+                <div className="logo">
+                  <Link to="/">
+                    <h1>ElhadjTALL</h1>
+                  </Link>
                 </div>
+              </div>
             </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-            {/*  Headet Botton */}
-            <div className="header-bottom">
-                <div className="container">
-                    <div className="header-wrapper">
-                        {/* Logo */}
-                        <div className="logo">
-                            <div className="logo-search-acte">
-                                <div className="logo">
-                                    <Link to="/">
-                                    <h1>ElhadjTALL</h1>
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </header>
-    )
-
-}
-
-
-export default Navitems
+export default NavItems;
