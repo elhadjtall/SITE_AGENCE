ce7c4d511f0bfda9e0e498b5d50225deded42a8e
Elhadj Tall <elhadjoumartall10@gmail.com>
2024-12-05 23:48:20 +0100

    Creat a navbar

--- frontend/src/components/NavItems.jsx
+++ frontend/src/components/NavItems.jsx
@@ -2,9 +2,31 @@ import React from "react";
 
 
 const NavItems = () => {
+    // On creer une constante pour le menu
+    const content = <>
+        <div>
+            <ul>
+                <Link to="Home">
+                    <li>Home</li>
+                </Link>
+                
+                <Link to="">
+                    <li>About</li>
+                </Link>
+                
+                <Link>
+                    <li>Services</li>
+                </Link>
+                
+                <Link>
+                    <li>Contact</li>
+                </Link>
+            </ul>
+        </div>
+    </>
     return (
         <nav>
-            
+
         </nav>
     )
 }
