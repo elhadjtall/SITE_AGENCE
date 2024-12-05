3d5c86f3bde6c813b4f3f96748fab727c0977ac6
Elhadj Tall <elhadjoumartall10@gmail.com>
2024-12-06 00:04:32 +0100

    Ajout du Tallwind pour la mise en forme Navbar

--- frontend/src/components/NavItems.jsx
+++ frontend/src/components/NavItems.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 
 const NavItems = () => {
@@ -10,15 +11,15 @@ const NavItems = () => {
                     <li>Home</li>
                 </Link>
                 
-                <Link to="">
+                <Link to="About">
                     <li>About</li>
                 </Link>
                 
-                <Link>
+                <Link to="Services">
                     <li>Services</li>
                 </Link>
                 
-                <Link>
+                <Link to="Contact">
                     <li>Contact</li>
                 </Link>
             </ul>
@@ -26,7 +27,33 @@ const NavItems = () => {
     </>
     return (
         <nav>
-
+            <div className="h-10vh flex justify-between z-50 text-white lg:py-5 px-20 py-4 flex-1">
+                <div className="flex items-center flex-1">
+                    <span className="text-3xl font-bold">Logo</span>
+                </div>
+                <div className="lg:flex md:flex lg: flex-1 items center justify-end font-normal hidden">
+                    <div className="flex-10">
+                        {/* ce code permet d'aligner de façons horizontal votre navbar */}
+                        <ul className="flex gap-8 mr-16 text-[18px]">
+                        <Link to="Home">
+                            <li>Home</li>
+                        </Link>
+                        
+                        <Link to="About">
+                            <li>About</li>
+                        </Link>
+                        
+                        <Link to="Services">
+                            <li>Services</li>
+                        </Link>
+                        
+                        <Link to="Contact">
+                            <li>Contact</li>
+                        </Link>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </nav>
     )
 }
