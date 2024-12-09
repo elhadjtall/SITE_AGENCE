1b877775b131676e78c15bc3587f3a62a787fe2a
Elhadj Tall <elhadjoumartall10@gmail.com>
2024-12-09 23:01:35 +0100

    Ajout du header nav

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
-
-
-// imporation de links
 import { Link } from 'react-router-dom';
 
-
-
-
-
 const Header = () => {
-  return <Header className='py-6 mb-12 border-b'>
-    <div className="container mx-auto flex justify-between items-center">
+  return (
+    <header className="py-6 mb-12 border-b">
+      <div className="container mx-auto flex justify-between items-center">
         {/* Logo */}
         <Link to="/">
-            <h3>MyImmo</h3>
+          <h3>MyImmo</h3>
         </Link>
-        {/* button */}
-        <div className='flex items-center gap-6'>
-            <Link className='hover:text-violet-900 transition-all duration-300' to='/'>Log In</Link>
-            <Link to='/'>Sign Up</Link>
+        {/* Buttons */}
+        <div className="flex items-center gap-6">
+          <Link
+            className="hover:text-violet-900 transition-all duration-300"
+            to="/"
+          >
+            Log In
+          </Link>
+          <Link className="hover:text-violet-900 transition-all duration-300" to="/">
+            Sign Up
+          </Link>
         </div>
-    </div>
-  </Header>;
+      </div>
+    </header>
+  );
 };
 
-export default Header
+export default Header;
