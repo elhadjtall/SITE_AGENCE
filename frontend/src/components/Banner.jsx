0d5b23bcb3ec7ad000723843d75df53baa4198e4
Elhadj Tall <elhadjoumartall10@gmail.com>
2024-12-09 23:37:21 +0100

    AJout des images du banneur

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -13,14 +13,14 @@ const Banner = () => {
     {/* le contenu du banner en 2 parties : le texte et l'image */}
           <div className='flex flex-col lg:flex-row'> 
             <div className='lg:ml-8 xl:ml-[135px] flex flex-col items-center lg:items-start text-center lg:text-left justify-center flex-1 px-4 lg:px-0'>
-              <h1><span>Trouver </span>une maison de rêve</h1>
-              <p>
+              <h1 className='text-4xl lg:text-[58px] font-semibold leading-none mb-6'><span className='text-violet-700'>Trouver </span>une maison de rêve</h1>
+              <p className='max-w-[480px] mb-8'>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
               </p>
             </div>
             {/* c'est l'image du banner importer en haut qu'on affiche ici*/}
-            <div>
-              <img src={banner} alt="" />
+            <div className='hidden lg:flex justify-end flex-1'>
+              <img className='max-h-[640px] w-full object-cover' src={banner} alt="" />
             </div>
           </div>
           <Search />
