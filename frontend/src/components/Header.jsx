cc47638f79481ad18a7b2ba132afcb0663ed473c
Elhadj Tall <elhadjoumartall10@gmail.com>
2024-12-09 21:21:49 +0100

    Modificaiton du nav

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -9,15 +9,15 @@ import { Link } from 'react-router-dom';
 
 
 const Header = () => {
-  return <Header>
-    <div className="container mx-auto">
+  return <Header className='py-6 mb-12 border-b'>
+    <div className="container mx-auto flex justify-between items-center">
         {/* Logo */}
         <Link to="/">
             <h3>MyImmo</h3>
         </Link>
         {/* button */}
-        <div>
-            <Link to='/'>Log In</Link>
+        <div className='flex items-center gap-6'>
+            <Link className='hover:text-violet-900 transition-all duration-300' to='/'>Log In</Link>
             <Link to='/'>Sign Up</Link>
         </div>
     </div>
