64634e51d70dab06eb14f6d29889206de87c3ac2
Elhadj Tall <elhadjoumartall10@gmail.com>
2024-12-11 23:35:47 +0100

    Ajout du context Provider

--- frontend/src/components/HouseContext.jsx
+++ frontend/src/components/HouseContext.jsx
@@ -6,9 +6,35 @@ export const HouseContext = createContext();
 
 // Composant Provider
 export const HouseContextProvider = ({ children }) => {
+  // Déclaration des états
+  const [houses, setHouses] = useState(housesData);
+  const [country, setCountry] = useState('location (any)');
+  const [countries, setCountries] = useState([]);
+  const [property, setProperty] = useState('property type (any)');
+  const [properties, setProperties] = useState([]);
+  const [price, setPrice] = useState('price range (any)');
+  const [loading, setLoading] = useState(false);
 
+  // Retourne le provider avec les valeurs du contexte
   return (
-    <HouseContext.Provider value={console.log('test') }>
+    <HouseContext.Provider
+      value={{
+        houses,
+        setHouses,
+        country,
+        setCountry,
+        countries,
+        setCountries,
+        property,
+        setProperty,
+        properties,
+        setProperties,
+        price,
+        setPrice,
+        loading,
+        setLoading,
+      }}
+    >
       {children}
     </HouseContext.Provider>
   );
