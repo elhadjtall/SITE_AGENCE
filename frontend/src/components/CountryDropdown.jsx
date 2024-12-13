d256ca17f37bfb6a26a1ae83e8613be4ad9d5336
Elhadj Tall <elhadjoumartall10@gmail.com>
2024-12-14 00:01:30 +0100

    Ajout de la barre des pays

--- frontend/src/components/CountryDropdown.jsx
+++ frontend/src/components/CountryDropdown.jsx
@@ -11,22 +11,39 @@ const CountryDropdown = () => {
   // console.log(country); // Affiche le pays actuel dans la console
 
   return (
+    // Code pour le menu de pays
     <Menu as="div" className="dropdown relative">
-      {/* Code pour le bouton de dropdown et ses icones */}
+      {/* Code pour le bouton de pays et la phrase "Selectionner votre pays"  */}
       <Menu.Button className="dropdown-btn w-full text-left" onClick={() => setIsOpen(!isOpen)}>
         <RiMapPinLine className="dropdown-icon-primary" />
         <div>
           <div className='text-[15px] font-medium leading-tight'>{country}</div>
           <div className='text-[13px]'>Selectionner votre pays</div>
-          {
-            isOpen ? (
-              <RiArrowUpSLine className="dropdown-icon-primary" />
-            ) : (
-              <RiArrowDownSLine className="dropdown-icon-primary" />
-            )
-          }
         </div>
+        {/* Code pour les icones de dropdown en haut et en bas */}
+        {isOpen ? (
+              <RiArrowUpSLine 
+              className="dropdown-icon-secondary" />
+            ) : (
+              <RiArrowDownSLine 
+              className="dropdown-icon-secondary" />
+            )}
     </Menu.Button>
+    {/* Code pour la liste de pays */}
+    <Menu.Items className="dropdown-menu">
+      {countries.map((country, index) => {
+        return (
+          <Menu.Item
+            as="li"
+            key={index}
+            onClick={() => setCountry(country)}
+            className="cursor-pointer hover:text-violet-700 transition"
+          >
+            {country}
+          </Menu.Item>
+        );
+      })}
+    </Menu.Items>
     </Menu>
   );
 };
