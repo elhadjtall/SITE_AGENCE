fd6fc4f8748bb5113e49081c6ae564e6d9c153b5
Elhadj Tall <elhadjoumartall10@gmail.com>
2024-12-15 20:59:05 +0100

    Ajout de la page property

--- frontend/src/components/PropertyDropdown.jsx
+++ frontend/src/components/PropertyDropdown.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState, useContext } from 'react'; // Suppression des imports inutilisés (useState, useEffect)
+import { RiMapPinLine, RiArrowDownSLine, RiArrowUpSLine } from 'react-icons/ri'; // Import des icônes
+import { Menu } from '@headlessui/react'; // Import de Headless UI
+import { HouseContext } from './HouseContext'; // Import du contexte
 
 const PropertyDropdown = () => {
-  return (
-    <div>
-      PropertyDropdown
-    </div>
-  )
-}
+  const { property, setProperty, properties } = useContext(HouseContext); // Utilisation du contexte
 
-export default PropertyDropdown
+  const [isOpen, setIsOpen] = useState(false);
+
+   console.log(properties); // Affiche le pays actuel dans la console
+
+  return (
+    // Code pour le menu de pays
+    <Menu as="div" className="dropdown relative">
+      {/* Code pour le bouton de pays et la phrase "Selectionner votre pays"  */}
+      <Menu.Button className="dropdown-btn w-full text-left" onClick={() => setIsOpen(!isOpen)}>
+        <RiMapPinLine className="dropdown-icon-primary" />
+        <div>
+          <div className='text-[15px] font-medium leading-tight'>{property}</div>
+          <div className='text-[13px]'>Selectionner votre property</div>
+        </div>
+        {/* Code pour les icones de dropdown en haut et en bas */}
+        {isOpen ? (
+              <RiArrowUpSLine 
+              className="dropdown-icon-secondary" />
+            ) : (
+              <RiArrowDownSLine 
+              className="dropdown-icon-secondary" />
+            )}
+    </Menu.Button>
+    {/* Code pour la liste de pays */}
+    <Menu.Items className="dropdown-menu">
+      {properties.map((property, index) => {
+        return (
+          <Menu.Item
+            as="li"
+            key={index}
+            onClick={() => setCountry(property)}
+            className="cursor-pointer hover:text-violet-700 transition"
+          >
+            {property}
+          </Menu.Item>
+        );
+      })}
+    </Menu.Items>
+    </Menu>
+  );
+};
+
+export default PropertyDropdown;
