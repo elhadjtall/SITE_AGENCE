0d92614df4b0b741558f1dbd2a582896df98a318
Elhadj Tall <elhadjoumartall10@gmail.com>
2024-12-15 22:13:34 +0100

    Ajout de Price RangeDropdown

--- frontend/src/components/PriceRangeDropdown.jsx
+++ frontend/src/components/PriceRangeDropdown.jsx
@@ -1,11 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState, useContext } from 'react'; // Suppression des imports inutilisés (useState, useEffect)
+import { RiWallet3Line, RiArrowUpSLine, RiHome5Line } from 'react-icons/ri'; // Import des icônes
+import { Menu } from '@headlessui/react'; // Import de Headless UI
+import { HouseContext } from './HouseContext'; // Import du contexte
 
 const PriceRangeDropdown = () => {
-  return (
-    <div>
-      PriceRangeDropdown
-    </div>
-  )
-}
+  const { price, setPrice} = useContext(HouseContext); // Utilisation du contexte
 
-export default PriceRangeDropdown
+  const [isOpen, setIsOpen] = useState(false);
+
+  //  console.log(properties); // Affiche le pays actuel dans la console
+  // definition du prix
+  const prices = [
+    {
+      value: 'price range (any)',
+    },
+    {
+      value: '$100,000 - $200,000',
+    },
+    {
+      value: '$200,000 - $400,000',
+    },
+    {
+      value: '$400,000 - $600,000',
+    },
+    {
+      value: '$600,000 - $800,000',
+    },
+    {
+      value: '$800,000 - $1,000,000',
+    },
+  ];
+
+  return (
+    // Code pour le menu de pays
+    <Menu as="div" className="dropdown relative">
+      {/* Code pour le bouton de pays et la phrase "Selectionner votre pays"  */}
+      <Menu.Button className="dropdown-btn w-full text-left" onClick={() => setIsOpen(!isOpen)}>
+        {/* Code pour les icones */}
+        <RiWallet3Line className="dropdown-icon-primary" />
+        <div>
+          <div className='text-[15px] font-medium leading-tight'>{price}</div>
+          <div className='text-[13px]'>Selectionner votre price</div>
+        </div>
+    </Menu.Button>
+    {/* Code pour la liste de pays */}
+    <Menu.Items className="dropdown-menu">
+      {prices.map((price, index) => {
+        return (
+          <Menu.Item
+            as="li"
+            key={index}
+            onClick={() => setPrice(price.value)}
+            className="cursor-pointer hover:text-violet-700 transition"
+          >
+            {price.value}
+          </Menu.Item>
+        );
+      })}
+    </Menu.Items>
+    </Menu>
+  );
+};
+
+export default PriceRangeDropdown;
