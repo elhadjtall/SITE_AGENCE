6619e77b6c2c42da88d2564f28c0b6a55c35f310
Elhadj Tall <elhadjoumartall10@gmail.com>
2024-12-16 22:53:05 +0100

    Ajout du fichier Hous.jsx et HouseListe.jsx

--- frontend/src/components/HouseList.jsx
+++ frontend/src/components/HouseList.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react';
+
+// import context
+import { HouseContext } from '../components/HouseContext';
+
+
+// impot components
+import House from '../components/House';
+
+// import links de navigation de react router
+import { Link } from 'react-router-dom';
+
+// import icons
+import { ImSpinner2 } from 'react-icons/im';
 
 const HouseList = () => {
+    const { houses, loading } = useContext(HouseContext)
+    console.log(houses)
   return (
     <div>
       HouseList
