17c75db7c16d673fb2eb6dc943614e8a1235fc4b
Elhadj Tall <elhadjoumartall10@gmail.com>
2024-12-16 22:38:08 +0100

    Ajout du fichier HouseList

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import React from "react";
 
 // Importation des composants
 import Banner from "../components/Banner";
+import HouseList from "../components/HouseList";
 import Footer from "../components/Footer";
 
 
@@ -11,6 +12,7 @@ const Home = () => {
   return (
     <div className="min-h-[1800px]">
       <Banner />
+      <HouseList />
     </div>
   )
 };
