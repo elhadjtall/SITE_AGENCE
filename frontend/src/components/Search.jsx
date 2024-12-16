0332b41248d510f58b92dc812e70af1568afa271
Elhadj Tall <elhadjoumartall10@gmail.com>
2024-12-16 21:39:47 +0100

    Importation du HouseContext dans le Search.jsx

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -16,7 +16,10 @@ import { RiSearch2Line } from 'react-icons/ri';
 
 // importation des HouseContext
 import { HouseContext } from './HouseContext';
+
 const Search = () => {
+  // importation de HouseContext 
+  const { handleClick } = useContext(HouseContext);
   return (
     <div className='px-[30px] py-6 max-w-[1170px] mx-auto flex flex-col lg:flex-row justify-between gap-4 lg:gap-x-3 relative lg:-top-4 lg:shadow-1 bg-red-50 lg:bg-transparent lg:backdrop-blur rounded-lg'>
       <CountryDropdown />
@@ -24,7 +27,7 @@ const Search = () => {
       <PriceRangeDropdown />
 
       {/* on affiche le bouton de recherche */}
-      <button className='bg-violet-700 hover:bg-violet-800 transition w-full lg:max-w-[162px] h-12 rounded-lg flex justify-center items-center text-white'>
+      <button onClick={() => handleClick()} className='bg-violet-700 hover:bg-violet-800 transition w-full lg:max-w-[162px] h-12 rounded-lg flex justify-center items-center text-white'>
         <RiSearch2Line />
       </button>
     </div>
