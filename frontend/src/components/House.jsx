b943777985a8e115b0c179a798a3fa3a14825b7d
Elhadj Tall <elhadjoumartall10@gmail.com>
2024-12-17 22:56:33 +0100

    Ajout du des infoes prix

--- frontend/src/components/House.jsx
+++ frontend/src/components/House.jsx
@@ -19,6 +19,9 @@ const House = ({ house}) => {
           <div className='bg-green-500 rounded-full text-white px-3'>{type}</div>
           <div className='bg-violet-500 rounded-full text-white px-3'>{country}</div>
       </div>
+      <div className='text-lg font-semibold max-w-[260px]'>{address}</div>
+      <div></div>
+      <div className='text-lg font-semibold text-violet-600 mb-4'>{price}</div>
     </div>
   )
 }
