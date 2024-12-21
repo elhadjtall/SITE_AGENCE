1c42746042fe33a8715077fb0fbacde01b1b8044
Elhadj Tall <elhadjoumartall10@gmail.com>
2024-12-21 23:56:50 +0100

    AJout not found

--- frontend/src/components/HouseList.jsx
+++ frontend/src/components/HouseList.jsx
@@ -26,7 +26,9 @@ const HouseList = () => {
 
     if (houses.length < 1) {
         return (
-          <ImSpinner2 className='mx-auto animate-spin text-violet-700 text-4xl mt-[200px]'/>  
+            <div className='text-center text-3xl text-gray-400 mt-48 font-medium'>
+                No properties found
+            </div> 
         );
     }
   return (
