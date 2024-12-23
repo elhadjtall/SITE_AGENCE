c644b1cf5bd406c1e766914626b4a9c759f38f3d
Elhadj Tall <elhadjoumartall10@gmail.com>
2024-12-23 23:12:15 +0100

    Ajout des details

--- frontend/src/pages/PropertyDetail.jsx
+++ frontend/src/pages/PropertyDetail.jsx
@@ -11,7 +11,13 @@ import { useParams } from 'react-router-dom';
 // import icons
 import { BiBed, BiBath, BiArea } from 'react-icons/bi';
 
+// import links de navigation de react router
+import { Link } from 'react-router-dom';
+
 const PropertyDetail = () => {
+    // recupere l'id de l'url
+    const { id } = useParams();
+    console.log(id);
   return (
     <div>
       PropertyDetails
