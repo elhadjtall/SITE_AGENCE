52a33aaa307b2990a22d8e1b7f715997c440e7f0
Elhadj Tall <elhadjoumartall10@gmail.com>
2024-12-23 23:07:47 +0100

    AJout des details

---
+++ frontend/src/pages/PropertyDetail.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+// importation des datas
+import { housesData } from '../data';
+
+
+// import use params
+import { useParams } from 'react-router-dom';
+
+// import icons
+import { BiBed, BiBath, BiArea } from 'react-icons/bi';
+
+const PropertyDetail = () => {
+  return (
+    <div>
+      PropertyDetails
+    </div>
+  )
+}
+
+export default PropertyDetail;
