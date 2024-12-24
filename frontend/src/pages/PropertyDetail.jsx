df49d9df1f14157d65e56d05fa0fa18b304cc0ed
Elhadj Tall <elhadjoumartall10@gmail.com>
2024-12-24 22:56:32 +0100

    Ajout de la description

--- frontend/src/pages/PropertyDetail.jsx
+++ frontend/src/pages/PropertyDetail.jsx
@@ -15,17 +15,26 @@ import { BiBed, BiBath, BiArea } from 'react-icons/bi';
 const PropertyDetail = () => {
   // Récupération de l'ID depuis les paramètres d'URL
   const { id } = useParams();
-  console.log(id);
 
   //get the house based on the id
   const house = housesData.find((house) => {
     return house.id === parseInt(id);
   });
-
+  
   return (
-    <div>
-        property detail
-    </div>
+    <section>
+        <div className='container mx-auto min-h-[800px] mb-14'>
+            <div>
+                <div>
+                    <h2>{house.name}</h2>
+                    <h2>{house.address}</h2>
+                </div>
+                <div>
+                    <div>{house.type}</div>
+                </div>
+            </div>
+        </div>
+    </section>
   );
 };
 
