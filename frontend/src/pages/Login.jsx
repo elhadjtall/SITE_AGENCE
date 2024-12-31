d51188d59c20889e7bc5a118a47c1efa40d642e5
Elhadj Tall <elhadjoumartall10@gmail.com>
2024-12-31 22:28:50 +0100

    Ajout de la page login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,18 +3,19 @@ import React from 'react'
 const Login = () => {
   return (
     <section>
-        <div className='container mx-auto flex flex-col lg:flex-row items-center justify-center min-h-screen gap-x-10 bg-violet-600'>
+        <div className='container mx-auto flex flex-col lg:flex-row items-center justify-center min-h-screen gap-4 bg-violet-700 px-4'>
             <div className='flex-1'>
-                <div className='text-white px-12 py-10'>
+                <div className='text-white px-12 py-10 text-center '>
                     <h1 className='text-6xl font-semibold mb-3'>Se connecter</h1>
-                    <p className='mb-6 text-4xl'>Vous pouvez vous connecter à votre compte</p>
+                    <p className='mb-6 text-4xl font-semibold'>Vous pouvez vous connecter à votre compte</p>
                 </div>
             </div>
             {/* form de connexion */}
-            <div className='flex-1 px-9 py-14 bg-white'>
+            <div className='flex-1 px-10 py-14 bg-white rounded w-full'>
+                <h1 className='text-3xl font-semibold mb-4 text-center py-4'>Log with account</h1>
                 <form className='flex flex-col gap-y-4'>
-                    <input className='border border-gray-300 focus:border-violet-700 outline-none rounded w-full px-4 h-14 text-sm' type="text" placeholder='Nom*' />
                     <input className='border border-gray-300 focus:border-violet-700 outline-none rounded w-full px-4 h-14 text-sm' type="email" placeholder='Email*' />
+                    <input className='border border-gray-300 focus:border-violet-700 outline-none rounded w-full px-4 h-14 text-sm' type="password" placeholder='Password*' />
                     <div className='flex gap-x-2'>
                         <button className='bg-violet-700 hover:bg-violet-800 text-white rounded p-4 text-sm w-full transition'>Se connecter</button>
                     </div>
