5a209a5886d255c2068ce9721173246d14de59f9
Elhadj Tall <elhadjoumartall10@gmail.com>
2024-12-31 21:36:36 +0100

    Ajout du login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <section>
-        <div>
-            <h1>Login</h1>
+    <section className='container mx-auto min-h-[600px] mb-14'>
+        <div className='text-center'>
+            <h1 className='text-3xl font-semibold mb-4'>Vous avez besoin de vous connecter</h1>
         </div>
+        <form className='flex flex-col gap-4 items-center'>
+            <label className='text-lg font-semibold' htmlFor="email">Email</label>
+            <input className='border border-gray-500 rounded-lg outline-none px-4 py-2' type="email" name="email" />
+            <label className='text-lg font-semibold' htmlFor="password">Mot de passe</label>
+            <input  className='border border-gray-500 rounded-lg outline-none px-4 py-2' type="password" name="password"  />
+            <button className='bg-violet-700 hover:bg-violet-800 transition text-white py-3 rounded-lg' type="submit">Se connecter</button>
+        </form>
     </section>
   )
 }
