e574d6da13fed18d96136ad11d28ba5a4b0ea557
Elhadj Tall <elhadjoumartall10@gmail.com>
2025-01-05 21:45:22 +0100

    Ajout de la page About

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -7,13 +7,13 @@ const About = () => {
   return (
     <section>
         <div>
-            <h1>Qui somme nous ?</h1>
+            <h1 className='text-3xl font-semibold'>Qui somme nous ?</h1>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur, mollitia fuga, quidem obcaecati possimus exercitationem sequi sint, alias quos aperiam omnis accusamus repudiandae quas! Quaerat quas quisquam eum eveniet officiis.</p>
         </div>
-        <div className='flex gap-x-8'>
-            <div className='flex flex-col'>
+        <div className='flex gap-x-8 '>
+            <div className='flex flex-col items-center'>
               <div className=''>
-                <img className='w-20 h-20 object-cover border border-gray-200 rounded-full' src={elon} alt="" />
+                <img className=' object-cover border border-gray-200' src={elon} alt="" />
               </div>
               <div>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>
