7aef5778be597b9ac6b3ed4c2bef80f52e9dc23e
Elhadj Tall <elhadjoumartall10@gmail.com>
2025-01-05 21:53:33 +0100

    Ajout de la page About

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -6,18 +6,19 @@ import elon from '../assets/img/elon.jpg';
 const About = () => {
   return (
     <section>
-        <div>
+        <div className='container mx-auto py-16 min-h-screen'>
+        <div className='text-center'>
             <h1 className='text-3xl font-semibold'>Qui somme nous ?</h1>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur, mollitia fuga, quidem obcaecati possimus exercitationem sequi sint, alias quos aperiam omnis accusamus repudiandae quas! Quaerat quas quisquam eum eveniet officiis.</p>
         </div>
-        <div className='flex gap-x-8 '>
+        <div className='flex gap-x-8 py-12 '>
             <div className='flex flex-col items-center'>
               <div className=''>
                 <img className=' object-cover border border-gray-200' src={elon} alt="" />
               </div>
               <div className='text-center flex gap-x-2'>
                 <h1 className='text-2xl font-semibold'>Elhadj TALL</h1>
-                <p className='text-2xl font-semibold'>CEO</p>
+                <p className='text-2xl font-semibold'> - CEO</p>
               </div>
               <div>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>
@@ -31,6 +32,7 @@ const About = () => {
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>
                 </div>
             </div>
+        </div>
         </div>
     </section>
   )
